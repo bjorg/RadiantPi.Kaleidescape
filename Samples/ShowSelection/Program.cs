8c4d32e71be21adc5536c45898c7e9a95776b2ea
Steve G. Bjorg <steve.bjorg@gmail.com>
2021-12-08 01:01:59 -0800

    added sample

---
+++ Samples/ShowSelection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RadiantPi.Kaleidescape;
+
+// check arguments
+if(args.Length != 1) {
+    Console.WriteLine("ERROR: missing Device ID as commandline argument");
+    return;
+}
+
+// initialize client
+using var client = new KaleidescapeClient(new() {
+    Host = "192.168.1.147",
+    Port = 10000,
+    DeviceId = args[0]
+});
+
+// hook-up event handlers
+client.HighlightedSelectionChanged += delegate (object? sender, HighlightedSelectionChangedEventArgs args) {
+    Console.WriteLine($"Selection: '{args.SelectionId}'");
+};
+
+// connect to device
+await client.ConnectAsync();
+
+// wait for exit
+Console.WriteLine("Connected to Kaleidescape. Press ENTER to exit.");
+Console.ReadLine();
